3a8755d6cfe0e72209a51cdb6914c2817ffb8430
Juan Jose Abuin <juanj.abuin@gmail.com>
2021-08-25 17:49:13 -0300

    Added refresh button on app menu bar

--- app/src/main/java/com/example/myforecast/activities/MainActivity.kt
+++ app/src/main/java/com/example/myforecast/activities/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.myforecast.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.domain.entities.DayWeatherInformation
@@ -35,6 +37,18 @@ class MainActivity : AppCompatActivity() {
         recyclerView.setHasFixedSize(true)
 
         onSearchClicked()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.refresh_button -> { onSearchClicked() }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun onSearchClicked() {
